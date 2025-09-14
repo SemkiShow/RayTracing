54ba0b5ab3dc45ce3fc29fdf5aeb651b8d02dcd4
SemkiShow <mgdeveloper123@gmail.com>
2025-09-14 10:27:00 +0200

    Add Camera.hpp

---
+++ include/Camera.hpp
@@ -0,0 +1,82 @@
+#pragma once
+
+#include "Color.hpp"
+#include "Hittable.hpp"
+#include <fstream>
+
+class Camera
+{
+  public:
+    double aspectRatio = 16.0 / 9;
+    int imageHeight = 480;
+
+    void render(const Hittable& world)
+    {
+        initialize();
+
+        std::ofstream output("image.ppm");
+        output << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
+
+        for (int i = 0; i < imageHeight; i++)
+        {
+            std::clog << "\rScanlines remaining: " << (imageHeight - i) << ' ' << std::flush;
+            for (int j = 0; j < imageWidth; j++)
+            {
+                Vector3 pixelCenter = pixel00Location + (j * pixelDeltaU) + (i * pixelDeltaV);
+                Vector3 rayDirection = pixelCenter - cameraCenter;
+                Ray ray(cameraCenter, rayDirection);
+
+                Color color = RayColor(ray, world);
+                WriteColor(output, color);
+            }
+        }
+        std::clog << "\rDone.                 \n";
+        output.close();
+    }
+
+  private:
+    int imageWidth;
+    Point3 cameraCenter;
+    Point3 pixel00Location;
+    Vector3 pixelDeltaU;
+    Vector3 pixelDeltaV;
+
+    void initialize()
+    {
+        // Image
+        imageWidth = imageHeight * aspectRatio;
+        imageHeight = std::max(1, imageHeight);
+
+        // Determine viewport dimensions.
+        double focalLength = 1.0;
+        double viewportHeight = 2.0;
+        double viewportWidth = viewportHeight * (imageWidth * 1.0 / imageHeight);
+        cameraCenter = Point3(0, 0, 0);
+
+        // Calculate the vectors across the horizontal and down the vertical viewport edges.
+        Vector3 viewportU = Vector3(viewportWidth, 0, 0);
+        Vector3 viewportV = Vector3(0, -viewportHeight, 0);
+
+        // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+        pixelDeltaU = viewportU / imageWidth;
+        pixelDeltaV = viewportV / imageHeight;
+
+        // Calculate the location of the upper left pixel.
+        Vector3 viewportUpperLeft =
+            cameraCenter - Vector3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
+        pixel00Location = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
+    }
+
+    Color RayColor(const Ray& ray, const Hittable& world) const
+    {
+        HitRecord rec;
+        if (world.hit(ray, Interval(0, infinity), rec))
+        {
+            return 0.5 * (rec.normal + Color(1, 1, 1));
+        }
+
+        Vector3 unitDirection = UnitVector(ray.direction());
+        double a = 0.5 * (unitDirection.y() + 1.0);
+        return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
+    }
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,73 +1,16 @@
-#include "Color.hpp"
-#include "Hittable.hpp"
+#include "Camera.hpp"
 #include "HittableList.hpp"
-#include "Interval.hpp"
-#include "Ray.hpp"
 #include "Sphere.hpp"
-#include "Utils.hpp"
-#include <fstream>
-
-Color RayColor(const Ray& ray, const Hittable& world)
-{
-    HitRecord rec;
-    if (world.hit(ray, Interval(0, infinity), rec))
-    {
-        return 0.5 * (rec.normal + Color(1, 1, 1));
-    }
-
-    Vector3 unitDirection = UnitVector(ray.direction());
-    double a = 0.5 * (unitDirection.y() + 1.0);
-    return (1.0 - a) * Color(1.0, 1.0, 1.0) + a * Color(0.5, 0.7, 1.0);
-}
 
 int main()
 {
-    // Image
-    double aspectRatio = 16.0 / 9;
-    int imageHeight = 480;
-    int imageWidth = imageHeight * aspectRatio;
-    imageHeight = std::max(1, imageHeight);
-
-    // World
     HittableList world;
     world.add(std::make_shared<Sphere>(Point3(0, 0, -1), 0.5));
     world.add(std::make_shared<Sphere>(Point3(0, -100.5, -1), 100));
 
-    // Camera
-    double focalLength = 1.0;
-    double viewportHeight = 2.0;
-    double viewportWidth = viewportHeight * (imageWidth * 1.0 / imageHeight);
-    Point3 cameraCenter = Point3(0, 0, 0);
+    Camera camera;
+    camera.aspectRatio = 16.0 / 9.0;
+    camera.imageHeight = 480;
 
-    // Calculate the vectors across the horizontal and down the vertical viewport edges.
-    Vector3 viewportU = Vector3(viewportWidth, 0, 0);
-    Vector3 viewportV = Vector3(0, -viewportHeight, 0);
-
-    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
-    Vector3 pixelDeltaU = viewportU / imageWidth;
-    Vector3 pixelDeltaV = viewportV / imageHeight;
-
-    // Calculate the location of the upper left pixel.
-    Vector3 viewportUpperLeft =
-        cameraCenter - Vector3(0, 0, focalLength) - viewportU / 2 - viewportV / 2;
-    Vector3 pixel00Location = viewportUpperLeft + 0.5 * (pixelDeltaU + pixelDeltaV);
-
-    std::ofstream output("image.ppm");
-    output << "P3\n" << imageWidth << ' ' << imageHeight << "\n255\n";
-
-    for (int i = 0; i < imageHeight; i++)
-    {
-        std::clog << "\rScanlines remaining: " << (imageHeight - i) << ' ' << std::flush;
-        for (int j = 0; j < imageWidth; j++)
-        {
-            Vector3 pixelCenter = pixel00Location + (j * pixelDeltaU) + (i * pixelDeltaV);
-            Vector3 rayDirection = pixelCenter - cameraCenter;
-            Ray ray(cameraCenter, rayDirection);
-
-            Color color = RayColor(ray, world);
-            WriteColor(output, color);
-        }
-    }
-    std::clog << "\rDone.                 \n";
-    output.close();
+    camera.render(world);
 }
